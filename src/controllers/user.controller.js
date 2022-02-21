598d212eda001be3371a00acb3543870d568771f
fontenele <henriquedevb@gmail.com>
2022-02-21 19:23:02 -0300

    feat: created endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,4 +5,38 @@ module.exports = {
     const results = await knex("users");
     return res.json(results);
   },
+  async create(req, res, next) {
+    const { username } = req.body;
+
+    try {
+      await knex("users").insert({ username });
+
+      res.status(201).send();
+    } catch (error) {
+      next(error);
+    }
+  },
+  async update(req, res, next) {
+    const { username } = req.body;
+    const { id } = req.params;
+
+    try {
+      await knex("users").update({ username }).where({ id });
+
+      res.status(200).send();
+    } catch (error) {
+      next(error);
+    }
+  },
+  async delete(req, res, next) {
+    const { id } = req.params;
+
+    try {
+      await knex("users").where({ id }).del();
+
+      res.status(200).send();
+    } catch (error) {
+      next(error);
+    }
+  },
 };
