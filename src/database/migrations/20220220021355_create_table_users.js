7a0b887fb057568fbb3131bd5736dfb12f2f6f7a
fontenele <henriquedevb@gmail.com>
2022-02-21 18:40:57 -0300

    feat: create migrations and seed to knex

---
+++ src/database/migrations/20220220021355_create_table_users.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id");
+    table.text("username").unique().notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
