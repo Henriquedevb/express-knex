9ebfec183bf4944ebb96e2b4a106b4dd188f6cd0
fontenele <henriquedevb@gmail.com>
2022-02-21 20:46:59 -0300

    feat: add .env

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,9 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      database: "knex_new",
-      user: "postgres",
-      password: "henrique",
+      database: process.env.SECRET_DB,
+      user: process.env.SECRET_USER,
+      password: process.env.SECRET_PASSWORD,
     },
     migrations: {
       tableName: "knex_migrations",
